8af5251a3f6756b3ebc6936e1963d8d4141b2736
John "0B1T" Romby <romby@outlook.com>
2019-11-01 22:12:03 +0100

    Added Angular Material and some basic setup

--- assignment2_2/fitness-web-app/src/app/app.module.ts
+++ assignment2_2/fitness-web-app/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/* Custom Components */
 import { AddWorkoutProgramComponent } from './components/add-workout-program/add-workout-program.component';
 import { EditWorkoutProgramComponent } from './components/edit-workout-program/edit-workout-program.component';
 import { WorkoutProgramsListComponent } from './components/workout-programs-list/workout-programs-list.component';
+
+/* Angular material */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material.module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -16,9 +23,13 @@ import { WorkoutProgramsListComponent } from './components/workout-programs-list
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
 export class AppModule { }
