f73d3c52df5dc29e37fff79d158df3adc3a6de95
John "0B1T" Romby <romby@outlook.com>
2019-11-08 10:45:53 +0100

    Getting ready for heroku

--- assignment2/backend/app.js
+++ assignment2/backend/app.js
@@ -90,7 +90,7 @@ app.use(function (req, res, next) {
   res.locals.error = req.flash('error');
 })
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 9000));
 app.listen(app.get('port'),function(){
   console.log('Server started on port: ' + app.get('port'));
 })
