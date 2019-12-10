0b4818e3b2429ff6b95e5f0faaef15333767070b
Max <maxbj@live.dk>
2019-12-10 21:21:00 +0100

    added start ass3

---
+++ assignment3/dualnback/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Login from './login/login'
+
+function App() {
+  return (
+
+    <React.Fragment>
+    <Login/>  
+   </React.Fragment>
+  )
+
+}
+
+
+export default App;
