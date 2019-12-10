66644ac28f9596fb71b22c95b507b4e98ff5dd4a
Max <maxbj@live.dk>
2019-12-10 22:04:09 +0100

    added game, fix dependencies husk npm install

--- assignment3/dualnback/src/App.js
+++ assignment3/dualnback/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Login from './login/login'
+import DuaDualnback from './game/Dualnback'
 
 function App() {
   return (
 
     <React.Fragment>
     <Login/>  
+    <DuaDualnback/> 
    </React.Fragment>
   )
 
